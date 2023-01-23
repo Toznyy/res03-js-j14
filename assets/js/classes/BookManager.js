49bd3e4bb645b476488b040feca9c1fc45696cc1
Toznyy <anthony.cormier29860@gmail.com>
2023-01-23 09:57:01 +0000

    ajout de la pause

--- assets/js/classes/BookManager.js
+++ assets/js/classes/BookManager.js
@@ -3,61 +3,72 @@ class BookManager {
     #books;
 
     constructor(books) {
-
         this.#books = [];
-        
+
     }
 
     get() {
-
         return this.#books;
     }
 
     set books(books) {
-
         this.#books = books;
 
     }
-    
+
     findAllBooks() {
-        
+        return this.#books;
     }
-    
+
     findBookById(id) {
-        
+        for (let i = 0; i < this.#books.length; i++) {
+            if (this.#books[i].id === id) {
+                return this.#books[i];
+            }
+            else {
+                return null;
+            }
+        }
     }
-    
+
     findBooksByTitle(title) {
-        
+
     }
-    
+
     findBooksByAuthor(author) {
-        
+
     }
-    
+
     findBooksByPublicationYear(year) {
-        
+
     }
-    
+
     createBook(book) {
-        
+
     }
-    
+
     deleteBook(bookId) {
-        
+
     }
-    
+
     editBook(book) {
-        
+
     }
-    
+
     save() {
-        let FirstBook = new Book();
-        
+        let jsonBook = JSON.stringify(books);
+        sessionStorage.setItem("books", jsonBook);
     }
-    
+
     load() {
-        
+        let booksStorage = JSON.parse(sessionStorage.getItem("books"));
+        let newBooks = [];
+
+        for (let i = 0; i < booksStorage.length; i++) {
+            let parseBook = JSON.parse(booksStorage[i]);
+            let newBook = new Book(parseBook.id, parseBook.title, parseBook.author, parseBook.publicationDate, parseBook.totalPages, parseBook.excerpt, parseBook.coverImage);
+            newBooks.push(newBook);
+        }
     }
 }
 
--- assets/js/classes/UserManager.js
+++ assets/js/classes/UserManager.js
@@ -48,11 +48,19 @@ class UserManager {
     }
     
     save() {
-        
+        let jsonUser = JSON.stringify(users);
+        sessionStorage.setItem("users", jsonUser);
     }
     
     load() {
+        let usersStorage = JSON.parse(sessionStorage.getItem("users"));
+        let newUsers = [];
         
+        for (let i = 0; i < usersStorage.length; i++) {
+            let parseUser = JSON.parse(booksStorage[i]);
+            let newUser = new User(parseUser.id, parseUser.username, parseUser.email, parseUser.password, parseUser.firstName, parseUser.lastName, parseUser.profileImage);
+            newUsers.push(newUser);
+        }
     }
 }
 
